421afc8ebce3b0c0439ade7e3c8327c9ef66d6d8
ometman <revomet@gmail.com>
2023-11-06 18:50:51 +0000

    Modify schema

--- schema.sql
+++ schema.sql
@@ -1,16 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE IF NOT EXISTS public.animals
-(
-    id integer NOT NULL,
+CREATE TABLE animals(
+    id integer PRIMARY KEY NOT NULL,
+    name text NOT NULL,
     date_of_birth date NOT NULL,
     escape_attempts integer NOT NULL,
     neutered boolean NOT NULL,
     weight_kg numeric NOT NULL,
-    CONSTRAINT animals_pkey PRIMARY KEY (id)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.animals
-    OWNER to postgres;
+);
